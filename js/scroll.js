41a170567df13d99f2e31ef152a9963b31217ce5
Oleg-chernov <razrab404@gmail.com>
2018-05-07 22:57:21 +0300

    Inital commit

---
+++ js/scroll.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.go').click( function(){
+
+    var scroll_el = $(this).attr('href');
+
+    if ($(scroll_el).length != 0) {
+        $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 700);
+    }
+        
+    return false;
+    });
+});
